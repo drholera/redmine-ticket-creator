527e77d95ea91b5be0dfba7f9d325e03dde5e9f4
drholera <drholera@gmail.com>
2020-04-10 11:33:31 +0300

    Small fixes

--- ticket_creator/env_handler/environment_base.py
+++ ticket_creator/env_handler/environment_base.py
@@ -3,7 +3,9 @@ import yaml
 
 class EnvironmentBase(object):
 
-    _config = []
+    __config = []
+    
+    __instance = ''
 
     def __init__(self, env_name):
         raise Exception("Do not create instance of EnvironmentBase class!")
@@ -11,6 +13,6 @@ class EnvironmentBase(object):
     def __set_config(self):
         with open('./config/config.yml', 'r') as stream:
             try:
-                self._config = yaml.safe_load(stream)
+                self.__config = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 print(exc)        
