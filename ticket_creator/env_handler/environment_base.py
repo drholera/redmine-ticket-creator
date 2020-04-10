983f0e5e08fb1b213252e25ffc116984def7790b
drholera <drholera@gmail.com>
2020-04-10 11:25:54 +0300

    Moved config to separate function

--- ticket_creator/env_handler/environment_base.py
+++ ticket_creator/env_handler/environment_base.py
@@ -6,11 +6,11 @@ class EnvironmentBase(object):
     _config = []
 
     def __init__(self, env_name):
+        raise Exception("Do not create instance of EnvironmentBase class!")
+
+    def __set_config(self):
         with open('./config/config.yml', 'r') as stream:
             try:
                 self._config = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
-                print(exc)
-
-        raise Exception("Do not create instance of EnvironmentBase class!")
-
+                print(exc)        
