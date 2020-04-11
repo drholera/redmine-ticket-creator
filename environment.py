240957ab3e776de80d3359c4b8040a823f026981
Dmitry Polkhov <drholera@gmail.com>
2020-04-11 23:22:04 +0300

    Started ticket creation functionality

--- environment.py
+++ environment.py
@@ -8,25 +8,26 @@ class Environment(object):
     """ Environment implementation. One for all """
     _config = []
     
-    _instance = ''
+    _instance = None
+    _url = None
+    _headers = None
 
     def __init__(self, env):
         self._instance = env
         self._config = self._init_config()
+        self._url = str(self._config['redmine_settings']['url'])
+        self._headers = {'X-Redmine-API-Key': str(self._config['redmine_settings']['api_key'])}
 
 
     def check_connection(self):
         """ Checking connection - retreiving list of tickets """
         try:
-            url = str(self._config['redmine_settings']['url'])
-            headers = {'X-Redmine-API-Key': str(self._config['redmine_settings']['api_key'])}
-
-            r = requests.get(url, headers=headers)
-            if r.status_code == 200:
+            res = requests.get(self._url + 'issues.' + self._config['api_format'], headers=self._headers)
+            if res.status_code == 200:
                 print(colored('Connection is: 200. OK.', 'green'))
                 return True
             
-            print(colored('Connection is: ' + r.status_code + '. KO', 'red'))
+            print(colored('Connection is: ' + res.status_code + '. KO', 'red'))
         except HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
         except ConnectionError as e:
@@ -34,6 +35,13 @@ class Environment(object):
 
         # If we don't have a positive result - return False.
         return False
+
+    def group_ticket_list(self):
+        full_url = self._url + 'issues.' + self._config['api_format'] + '?assigned_to_id=' + self._config['deployment_groups'][self._instance]
+        res = requests.get(full_url, headers=self._headers)
+
+        # todo: add handlers for the response.
+        print(res.content)
 
     @staticmethod
     def get_config():
