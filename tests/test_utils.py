74ae9eb4e85c1fd2217511df0828580349cb67b2
Dmitry Polkhov <drholera@gmail.com>
2020-04-15 00:05:50 +0300

    Added coverage for parsing method

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,14 @@
-import sys, os
+from utils import Utils
+import requests_mock
+import unittest.mock as mock
+import sys
+import os
+import requests
+from xml.parsers.expat import ExpatError
+from json.decoder import JSONDecodeError
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
-import unittest.mock as mock
-from utils import Utils
 
 def test_get_issues_list():
     u = Utils()
@@ -14,26 +19,70 @@ def test_get_issues_list():
     assert u.get_issues_list(json_list) == [1, 2]
     assert u.get_issues_list(xml_list, 'xml') == [1, 2]
 
+
 def test_prompt(capsys):
     u = Utils()
 
-    with mock.patch('builtins.input', return_value = 'y'):
+    with mock.patch('builtins.input', return_value='y'):
         assert u.prompt("Positive?") == True
         captured = capsys.readouterr()
         assert captured.out == 'Positive? [y/n]: '
 
-    with mock.patch('builtins.input', return_value = 'yes'):
+    with mock.patch('builtins.input', return_value='yes'):
         assert u.prompt("Full yes input") == True
 
     # Clear stdout before next assert.
     captured = capsys.readouterr()
-    with mock.patch('builtins.input', return_value = 'asdasd'):
+    with mock.patch('builtins.input', return_value='asdasd'):
         assert u.prompt("Random input") == False
         captured = capsys.readouterr()
         assert captured.err == 'Unexpected input. Shutting down.'
 
-    with mock.patch('builtins.input', return_value = 'n'):
+    with mock.patch('builtins.input', return_value='n'):
         assert u.prompt("Negative?") == False
 
-    with mock.patch('builtins.input', return_value = 'not'):
+    with mock.patch('builtins.input', return_value='not'):
         assert u.prompt("Negative full?") == False
+
+
+def test_parse_response():
+    u = Utils()
+    test_json = {
+        'issue': {
+            "id": "1",
+            "title": "Test title"
+        }
+    }
+
+    test_xml = '''<?xml version="1.0" encoding="UTF-8" ?>
+                    <issue>
+                        <id>1</id>
+                        <title>Test title</title>
+                    </issue>'''
+    
+    with requests_mock.Mocker() as m:
+        m.register_uri('GET', 'http://test_json.com', json=test_json)
+        assert u.parse_response(requests.get('http://test_json.com')) == test_json
+        assert u.parse_response(requests.get('http://test_json.com'), 'json') == test_json
+
+        # Trying to parse Json as XML.
+        try :
+            u.parse_response(requests.get('http://test_json.com'), 'xml') != test_json
+            assert False
+        except ExpatError:
+            assert True
+
+        m.register_uri('GET', 'http://test_xml.com', text=test_xml)
+        # Return dict must be the same is test_json variable.
+        assert u.parse_response(requests.get('http://test_xml.com'), 'xml') == test_json
+
+        # Trying to parse Json as XML.
+        try :
+            u.parse_response(requests.get('http://test_xml.com'), 'json') == test_json
+            u.parse_response(requests.get('http://test_xml.com')) == test_json
+            assert False
+        except JSONDecodeError:
+            assert True
+
+        m.register_uri('GET', 'http://test_text.com', content=bytes('Some text', 'utf-8'))
+        assert u.parse_response(requests.get('http://test_text.com'), 'text') == bytes('Some text', 'utf-8')
