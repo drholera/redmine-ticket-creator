5ee5631831bef53e5602f733a3595291a977f515
Dmitry Polkhov <drholera@gmail.com>
2020-04-14 22:50:11 +0300

    Updated test vars name

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,34 +6,34 @@ import unittest.mock as mock
 from utils import Utils
 
 def test_get_issues_list():
-    utils = Utils()
+    u = Utils()
     json_list = {'issues': [1, 2]}
     xml_list = {'issues': {'issue': [1, 2]}}
 
-    assert utils.get_issues_list(json_list, 'json') == [1, 2]
-    assert utils.get_issues_list(json_list) == [1, 2]
-    assert utils.get_issues_list(xml_list, 'xml') == [1, 2]
+    assert u.get_issues_list(json_list, 'json') == [1, 2]
+    assert u.get_issues_list(json_list) == [1, 2]
+    assert u.get_issues_list(xml_list, 'xml') == [1, 2]
 
 def test_prompt(capsys):
-    utils = Utils()
+    u = Utils()
 
     with mock.patch('builtins.input', return_value = 'y'):
-        assert utils.prompt("Positive?") == True
+        assert u.prompt("Positive?") == True
         captured = capsys.readouterr()
         assert captured.out == 'Positive? [y/n]: '
 
     with mock.patch('builtins.input', return_value = 'yes'):
-        assert utils.prompt("Full yes input") == True
+        assert u.prompt("Full yes input") == True
 
     # Clear stdout before next assert.
     captured = capsys.readouterr()
     with mock.patch('builtins.input', return_value = 'asdasd'):
-        assert utils.prompt("Random input") == False
+        assert u.prompt("Random input") == False
         captured = capsys.readouterr()
         assert captured.err == 'Unexpected input. Shutting down.'
 
     with mock.patch('builtins.input', return_value = 'n'):
-        assert utils.prompt("Negative?") == False
+        assert u.prompt("Negative?") == False
 
     with mock.patch('builtins.input', return_value = 'not'):
-        assert utils.prompt("Negative full?") == False
+        assert u.prompt("Negative full?") == False
