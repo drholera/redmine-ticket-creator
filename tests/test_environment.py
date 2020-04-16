7e24f048d98621e8ede18c9f001ef15231cd7479
Dmitry Polkhov <drholera@gmail.com>
2020-04-17 00:32:36 +0300

    Small update for tests

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -3,12 +3,20 @@ import environment
 import mockfs
 import yaml
 from unittest.mock import patch
+import requests_mock
+import requests
+import io
+import pytest
 
 
 class TestEnvironment(unittest.TestCase):
 
     def setUp(self):
         self.env = environment.Environment('dev')
+
+    @pytest.fixture(autouse=True)
+    def capfd(self, capsys):
+        self.capsys = capsys
 
     def test_config(self):
         self.assertTrue(len(self.env._config) > 0)
