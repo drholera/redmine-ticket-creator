a1466fd813c8789930b07df52bec6a080436a631
Dmitry Polkhov <drholera@gmail.com>
2020-04-16 23:45:23 +0300

    Added possibility to mock filesystem

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,13 +1,18 @@
 import unittest
 import environment
+import mockfs
+import yaml
+from unittest.mock import patch
+
 
 class TestEnvironment(unittest.TestCase):
 
     def setUp(self):
-        self.env = environment.Environment('dev') 
+        self.env = environment.Environment('dev')
 
     def test_config(self):
         self.assertTrue(len(self.env._config) > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
